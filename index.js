e92e086e5cd3e436de9a2d44fa1d03d7dd0a7b6d
Max Gomes <maximiliano.pinto@klarna.com>
2021-12-06 19:04:02 +0100

    Add paranaue

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function scrollRight() {
-  const carousel = document.getElementById('carousel')
-  carousel.scrollLeft = carousel.scrollLeft + 100
+  const carousel = document.getElementById("carousel");
+  carousel.scrollLeft = carousel.scrollLeft + 100;
 }
-  
+
+function goTo(url) {
+  window.location.href = url;
+}
